156a9384fd3fa161a274d3b390d173aff8d5ac74
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2020-05-11 17:16:41 +0100

    Added more scenery

--- scs/dizzy.js
+++ scs/dizzy.js
@@ -37,6 +37,36 @@ export function createScenery(scene, loader) {
 	);
 
 
+	// Ceiling
+	loader.load('textures/dizzy3/floor4.png',
+		texture => {
+			texture.wrapS = THREE.NearestFilter;
+			texture.wrapT = THREE.NearestFilter;
+			texture.magFilter = THREE.NearestFilter;
+			texture.minFilter = THREE.NearestFilter;
+			texture.repeat.x = 4;
+			texture.repeat.y = 4;
+			
+			var woodMaterial = new THREE.MeshPhongMaterial({
+				map: texture,
+				//transparent: true
+			});
+
+			var floor = new THREE.Mesh(
+			  new THREE.PlaneGeometry(WIDTH, DEPTH),
+			  woodMaterial
+			);
+			floor.rotation.x = Math.PI / 2;
+			
+			floor.position.x = 0;
+			floor.position.y = 4;
+			floor.position.z = 1-(WIDTH/2);
+			//floor.receiveShadow = true;
+			scene.add(floor);
+		}
+	);
+
+
 	// Back wall
 	loader.load('textures/dizzy3/floor5.png',
 		texture => {
@@ -53,56 +83,65 @@ export function createScenery(scene, loader) {
 			});
 
 			var floor = new THREE.Mesh(
-			  new THREE.PlaneGeometry(WIDTH, 3),
+			  new THREE.PlaneGeometry(WIDTH, 4),
 			  woodMaterial
 			);
 			
 			//floor.position.x = -2;
-			floor.position.y = 1.5;
+			floor.position.y = 2;
 			floor.position.z = 1-DEPTH;
 
 			scene.add(floor);
 		}
 	);
 
-/*
-		// Add floor
-		createBillboard(scene, loader, 'textures/dizzy3/floor4.png', 4, 4, function(floor) {
-			floor.position.z = -2;
-			floor.position.z = -2;
-			floor.position.y = 0;
 
-			floor.rotation.x = -Math.PI / 2;
-			scene.add(floor);
-		});
-		createBillboard(scene, loader, 'textures/dizzy3/floor4.png', 4, 4, function(floor) {
-			floor.position.z = -6;
-			floor.position.y = 0;
+	// Barrel
+	createBillboard(scene, loader, 'textures/dizzy3/barrel.png', 2, 2, function(floor) {
+		floor.position.x = -2;
+		floor.position.y = 1;
+		floor.position.z = -4;
+		scene.add(floor);
+		
+		floor.components = [];
+		floor.components["face"] = true;
+		//entities.add(scene);
+	});
+	
+	// Table
+	createBillboard(scene, loader, 'textures/dizzy3/table.png', 2, 2, function(floor) {
+		floor.position.x = 2;
+		floor.position.y = 1;
+		floor.position.z = -4;
+		scene.add(floor);
+		
+		floor.components = [];
+		floor.components["face"] = true;
+		//entities.add(scene);
+	});
 
-			floor.rotation.x = -Math.PI / 2;
-			scene.add(floor);
-		});
+	// Skeleton
+	createBillboard(scene, loader, 'textures/dizzy3/hangingskeleton.png', 1, 3, function(floor) {
+		floor.position.x = -2;
+		floor.position.y = 2;
+		floor.position.z = 1-DEPTH+.1;
+		scene.add(floor);
+		
+		floor.components = [];
+		floor.components["face"] = true;
+		//entities.add(scene);
+	});
+	
+	// Flame torch
+	createBillboard(scene, loader, 'textures/dizzy3/flametorch.png', .5, 1, function(floor) {
+		floor.position.x = 2;
+		floor.position.y = 2;
+		floor.position.z = 1-DEPTH+.1;
+		scene.add(floor);
+		
+		floor.components = [];
+		floor.components["face"] = true;
+		//entities.add(scene);
+	});
 
-		// Wall
-		createBillboard(scene, loader, 'textures/dizzy3/floor5.png', 4, 4, function(floor) {
-			floor.position.z = -8;
-			floor.position.y = 0;
-
-			//floor.rotation.x = -Math.PI / 2;
-			scene.add(floor);
-		});
-*/
-
-
-		// Barrel
-		createBillboard(scene, loader, 'textures/dizzy3/barrel.png', 2, 2, function(floor) {
-			floor.position.x = -2;
-			floor.position.y = 1;
-			floor.position.z = -4;
-			scene.add(floor);
-			
-			floor.components = [];
-			floor.components["face"] = true;
-			//entities.add(scene);
-		});
 }
