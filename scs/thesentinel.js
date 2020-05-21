0630777a54f9f2f5e0f893f3a244d6a0f85475c0
Stephen Carlyle-Smith <StephenCS@ecmsys.local>
2020-05-21 15:51:02 +0100

    Added map gen code

--- scs/thesentinel.js
+++ scs/thesentinel.js
@@ -1,8 +1,40 @@
 import * as THREE from '../build/three.module.js';
 import { createCuboid } from './helperfunctions.js';
-//import { getRandomInt } from './numberfunctions.js';
+import { getRandomInt } from './numberfunctions.js';
 //import { create2DArray } from './collections.js';
 import { createCuboidSides} from './helperfunctions.js';
+
+
+export function generateMapData() {
+		const SIZE = 40;
+		map = create2DArray(SIZE); // Array of heights of corners of each plane
+		mapflat = create2DArray(SIZE); // Array of heights of corners of each plane
+		for (var z=0 ; z<SIZE-1 ; z++) {
+			for (x=0 ; x<SIZE-1 ; x++) {
+				var rnd = 0;//getRandomInt(0, 4);
+				map[x][y] = rnd;
+			}
+		}
+		
+		for (var i=0 ; i<10 ; i++) {
+				var x = getRandomInt(0, SIZE-1);
+				var z = getRandomInt(0, SIZE-1);
+				var rad  = getRandomInt(2, 10);
+				raiseMap(map, x, z, rad);
+		}
+		
+		return map;
+}
+
+
+function raiseMap(map, sx, sz, rad) {
+		for (var z=sz ; z<sz+rad ; z++) {
+			for (var x=sx ; x<sx+rad ; x++) {
+				map[x][y]++;
+			}
+		}
+}
+
 
 export function createMap(map, SIZE) {
 		var geom = new THREE.Geometry();
@@ -41,6 +73,7 @@ export function createMap(map, SIZE) {
 		end.name = "Global_map";
 		return end;
 }
+
 
 export function createCube_OLD(loader, callback) {
 	createCuboid(loader, 'textures/thesentinel/lavatile.jpg', .45, function(cube) {		
